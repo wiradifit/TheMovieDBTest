2de9aad7213ebd5f6dd4500cdac9421cfd358dbf
fitrajayaprawira <fitrajayaprawira@gmail.com>
2022-11-24 14:41:02 +0700

    initial commit

---
+++ GLITechnicalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GLITechnicalTest
+//
+//  Created by Prawira Hadi Fitrajaya on 24/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GLITechnicalTestUITests/GLITechnicalTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GLITechnicalTestUITestsLaunchTests.swift
+//  GLITechnicalTestUITests
+//
+//  Created by Prawira Hadi Fitrajaya on 24/11/22.
+//
+
+import XCTest
+
+final class GLITechnicalTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
